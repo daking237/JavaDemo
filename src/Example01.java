d9a382dbf86d2e22b6fb785af72d8c597659d997
Daking237 <chigodlove1@gmail.com>
2022-06-24 23:08:57 +0100

    Second Commit

--- src/Example01.java
+++ src/Example01.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Example01 {
     public static void main(String[] args){
-        System.out.println("hello World");
+        //MortgageCalculator
+        final int PERCENT =100;
+        final int MONTH =12;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter principal: ");
+        int principal = scanner.nextInt();
+
+        System.out.print("Annual Interest Rate: ");
+        float interestRate = scanner.nextFloat();
+        float monthlyRate = interestRate / PERCENT / MONTH;//r
+
+        System.out.print("Duration(years): ");
+        int Years = scanner.nextInt();
+        int NumberOfPayments= Years * MONTH;//n
+
+        double Mortgage = principal *(monthlyRate *Math.pow(1+ monthlyRate ,NumberOfPayments)/ Math.pow(1 + monthlyRate,NumberOfPayments)-1);
+
     }
 }
